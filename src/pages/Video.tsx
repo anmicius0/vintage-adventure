3678161128a25d519e2622a6967790bbf5e776d8
Anmicius <anmicius@disroot.org>
2024-05-18 19:13:21 +0800

    New confetti

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,19 +1,53 @@
 import { useLocation } from "react-router-dom";
 import { IonButton, IonCard } from "@ionic/react";
 import { useEffect, useState } from "react";
-import useWindowSize from "react-use/lib/useWindowSize";
-import Confetti from "react-confetti";
+import confetti from "canvas-confetti";
 
 interface VideoProps {
   setTitle: (title: string) => void;
   setProgress: (progress: number) => void;
 }
 const Video: React.FC<VideoProps> = ({ setTitle, setProgress }) => {
-  const { width, height } = useWindowSize();
+  // Confetti
+  var count = 200;
+  var defaults = {
+    origin: { y: 0.7 },
+  };
+
+  function fire(particleRatio: number, opts: Object) {
+    confetti({
+      ...defaults,
+      ...opts,
+      particleCount: Math.floor(count * particleRatio),
+    });
+  }
 
   useEffect(() => {
     setTitle("完成");
     setProgress(1);
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
   }, []);
 
   const {
@@ -50,8 +84,6 @@ const Video: React.FC<VideoProps> = ({ setTitle, setProgress }) => {
 
   return (
     <>
-      <Confetti width={width} height={height} recycle={false} />
-
       <div
         style={{
           display: "flex",
